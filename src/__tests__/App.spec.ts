810e23a1664b5fb58cfc822792503bc4ac7cb199
chameleon05119 <chameleon05119@gmail.com>
2024-01-28 16:19:56 +0900

    ユーザーの入力によって望ましい値が表示されるか確認するテストの追加

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -11,4 +11,14 @@ describe("App.vueのテスト", () => {
     const expected = "2";
     expect(actual).toBe(expected);
   });
+  test("分子に8を分母に2を指定した場合のテスト", () => {
+    async () => {
+      const wrapper = mount(App);
+      await wrapper.get(`[data-testid="num1"]`).setValue(8);
+      await wrapper.get(`[data-testid="num2"]`).setValue(2);
+      const actual = wrapper.get(`[data-testid="ans"]`).text();
+      const expected = "4";
+      expect(actual).toBe(expected);
+    };
+  });
 });
