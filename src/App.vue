bdde0cee64852a0d8d0c6e6fb9d06b595cc51dd4
chameleon05119 <chameleon05119@gmail.com>
2024-01-28 16:31:34 +0900

    ボタンによる要素の表示・非表示をテストするためのコードを追加

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,38 @@ const num2 = ref(3);
 const ans = computed((): number => {
   return divideTwoNums(num1.value, num2.value);
 });
+
+const isVisible = ref(false);
+const onShowButtonClick = (): void => {
+  isVisible.value = true;
+};
 </script>
 
 <template>
-  <p>
-    <input type="number" data-testid="num1" v-model="num1" />
-    ÷
-    <input type="number" data-testid="num2" v-model="num2" />
-    =
-    <span data-testid="ans">{{ ans }}</span>
-  </p>
+  <div>
+    <h2>計算結果を表示する要素のテスト用</h2>
+    <p>
+      <input type="number" data-testid="num1" v-model="num1" />
+      ÷
+      <input type="number" data-testid="num2" v-model="num2" />
+      =
+      <span data-testid="ans">{{ ans }}</span>
+    </p>
+  </div>
+  <br />
+  <div>
+    <h2>要素の表示・非表示のテスト用</h2>
+    <p>
+      隠し領域を<button data-testid="showButton" @click="onShowButtonClick">
+        表示
+      </button>
+      する
+    </p>
+    <p v-if="isVisible" data-testid="invisible-v-if">
+      隠し領域が表示されました
+    </p>
+    <p v-show="isVisible" data-testid="invisible-v-show">
+      隠し領域が表示されました
+    </p>
+  </div>
 </template>
