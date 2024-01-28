420c42e0ab1d2a765e87d9fb7a1cd483b41dc4f0
chameleon05119 <chameleon05119@gmail.com>
2024-01-28 16:06:09 +0900

    App.vueの初期表示のテストを追加

---
+++ src/__tests__/App.spec.ts
@@ -0,0 +1,14 @@
+import { describe, test, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import App from "@/App.vue";
+
+describe("App.vueのテスト", () => {
+  test("初期値での表示内容テスト", () => {
+    // App.vueの擬似mount
+    const wrapper = mount(App);
+    // pタグ要素のレンダリング結果を取り出す
+    const actual = wrapper.get("p").text();
+    const expected = "6 ÷ 3 = 2";
+    expect(actual).toBe(expected);
+  });
+});
